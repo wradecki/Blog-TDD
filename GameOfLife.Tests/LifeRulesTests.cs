6af027303dc695b77a012aedfc26e5e9401ddc1b
Wiktor Radecki <wiktor.radecki@mail.com>
2015-02-05 22:26:41 +0100

    Add test for Any dead cell wit exactly three live neighbours becomes a live cell

---
+++ GameOfLife.Tests/LifeRulesTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace GameOfLife.Tests
+{
+    public class LifeRulesTests
+    {
+        // When dead cell with exactly 3 live neightburs Then return alive
+        [Fact]
+        public void When_dead_cell_with_exactly_3_live_neightburs＿＿Then_return_alive()
+        {
+            CellState currentState = CellState.Dead;
+            LifeRules sut = new LifeRules();
+            var liveNeighbours = 3;
+
+            CellState actual = sut.GetNewState(currentState, liveNeighbours);
+            
+            CellState expected = CellState.Alive;
+
+            actual.Should().Be(expected);
+        }
+    }
+}
