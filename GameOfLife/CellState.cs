fd328da1e50d2680c5f499be41fbf01331ac15f3
Wiktor Radecki <wiktor.radecki@mail.com>
2015-02-05 23:25:38 +0100

    Make compile

---
+++ GameOfLife/CellState.cs
@@ -0,0 +1,8 @@
+namespace GameOfLife
+{
+    public enum CellState
+    {
+        Dead,
+        Alive
+    }
+}
---
+++ GameOfLife/LifeRules.cs
@@ -0,0 +1,10 @@
+namespace GameOfLife
+{
+    public class LifeRules
+    {
+        public CellState GetNewState(CellState currentState, int liveNeighbours)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
